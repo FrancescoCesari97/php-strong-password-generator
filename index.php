10d7f15d01925e74ae8a38c16f29c250b66959d3
FrancescoCesari97 <freedus97@gmail.com>
2024-02-28 15:41:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+
+
+$sent_pasw = !empty($_GET);
+
+
+if($sent_pasw){
+
+
+    $pasw_leght = $_GET['password-leght'];
+
+    var_dump($pasw_leght);
+}
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h1>Geratore password</h1>
+
+
+        <div class="">
+
+        </div>
+
+
+        <form action="" method="GET" class="row">
+            <div class="col-10">
+                <input type="number" class="form-control" name="password-leght" autofocus>
+
+            </div>
+            <div class="col-2">
+                <button class="btn btn-primary ">invia</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
